dd1d31de496c43b21be81bc2ac1eb7ef87b41231
spartanJS <alexandrephe@gmail.com>
2018-12-03 22:13:31 +0100

    Version : v0.0.2 alpha date : 3 december 2018 description : Flask API "MVP" for Medsense API Packages : flask, flask_restplus Tasks : - Create the light DTO & DAO for Medsense API         - Create a Blank Page with a Button to POST data on API

--- apimed.py
+++ apimed.py
@@ -2,6 +2,26 @@
 # -*-coding:Utf-8 -*
 """ Infos : Medsense API
 
+version : v0.0.2 alpha
+date :
+description : Flask API "MVP" for Medsense API
+Packages : flask, flask_restplus
+Tasks : - Create the light DTO & DAO for Medsense API
+        - Create a Blank Page with a Button to POST data on API
+Endpoints : - GET /apiv1/responses
+            - GET /apiv1/responses/<responses_id>
+Issues : ?
+Steps : 1) Canvas of the Flask/flask_restplus
+        2) Define the endpoints GET/GETALL
+        3) Define the DAO (Get/create/update/delete) to point the examples (replaced after with DB)
+        4) Define the DTO/Model
+        5) Define the others endpoints POST/UPDATE/DELETE + Define DAO DTO for Update Delete
+
+Important : Pour afficher "joliment" on utilise "skip_none" = True
+Si on souhaite bosser correctement sur le model (Versions ultérieures
+on devrait passer "skip_none" = False
+
+------------
 version : v0.0.1
 description : Flask API Canvas for Medsense API
 packages : flask, flask_restplus
@@ -10,26 +30,134 @@ tasks : - create a canvas
         - define the endpoint : /api/v1/responses/<responses_id>
         - start a good docstring
 Issue : curl answer don't take accent on french words
-
 """
+from collections import OrderedDict
+import requests as rq
+import json
 
-from flask import Flask
-from flask_restplus import Api, Resource, reqparse, abort
+
+from flask import Flask, request, render_template
+from flask_restplus import Resource, Api, fields, marshal_with, marshal, reqparse, abort
+from werkzeug.contrib.fixers import ProxyFix
+
+import data #Contains the data examples : 'data.alex & data.aline'
+
 
 app = Flask(__name__)
-api = Api(app)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app, version='0.0.2', title='Medsense API', description='Medsense collecting patient data API')
 
-parser = reqparse.RequestParser()
-parser.add_argument('data') #Here, the arg is the field that we want
+#Namespace is apiv1 for all the first version
+ns = api.namespace('apiv1',description='Medsense Patient CRUD')
+
+#Permet d'utiliser le kwarg skip_none = True dans marshal_with
+null = None
+
+#parser = reqparse.RequestParser()
+#parser.add_argument('data') #Here, the arg is the field that we want
 
 #MODIFY : initial survey_responses
 #We can also name survey_responses : responses
+""" Useful to test directly API Endpoints with a responses_dict - without DAO/DTO """
+"""
 responses_dict = {
-    'resp1': {'data': 'Pas du tout'},
-    'resp2': {'data': 'Tres peu'},
-    'resp3': {'data': 'Pas du tout'}
+    'resp1': data.alex,
+    'resp2': data.aline,
+}
+"""
+
+""" DTO Object
+
+Formating the Json that we received
+Responses DTO Model :
+{
+    id : string
+    href : Url
+    questions : [ {
+        id : String
+        text : String
+        answer_id : String
+        answer_text : String
+        answer_score : Integer
+                    }]
 }
 
+"""
+questions_fields = api.model('Questions', {
+    'id': fields.String(attribute='q_id'),
+    'text': fields.String,
+    'answer_id': fields.String,
+    'answer_text': fields.String,
+    'answer_score': fields.Integer,
+})
+responses_fields = api.model('ResponsesDTO',
+    {
+        'id': fields.String(attribute='responses_id'),
+        'href': fields.Url('responses_ep'),
+        'questions': fields.List(fields.Nested(questions_fields, skip_none=True)),
+    })
+
+
+class ResponsesDAO(object):
+    """ DAO Object : Responses
+
+    Data access object for patient responses
+
+    Methods :
+    GET -- Return the object
+    CREATE -- Create the object
+    UPDATE -- Modify the Object
+    DELETE -- Delete the object
+
+    Infos : V 0.0.2
+    -- The object that we created is just a concatenation of all responses object
+    -- To know the "param" of the object "responses" we need the DTO/model
+    -- Usefull to increment the creation of "responses"
+    """
+    def __init__(self):
+        """ Initialisation of the ResponsesDAO """
+        self.counter = 0
+        self.responses = []
+
+    def get(self,id):
+        """ Get a "responses" from the ResponsesDAO(object)
+
+        parameters :
+        resp -- an element in responses
+
+        """
+        for resp in self.responses:
+            if resp['responses_id'] == id:
+                return resp
+        api.abort(404, f"Response : {id} doesn t exist")
+
+    def create(self,data):
+        """ Create a "responses" in the ResponsesDAO(Object) """
+        """ Responses endpoint shape  : "resp1","resp2"... """
+        resp = data
+        self.counter = self.counter + 1
+        resp['responses_id'] = f"resp{self.counter}"
+        self.responses.append(resp)
+        return resp
+
+    def update(self, id, data):
+        """ Update a "responses" in ResponsesDAO"""
+        resp = self.get(id)
+        resp.update(data)
+        return resp
+
+    def delete(self,id):
+        """ Delete a "responses" in ResponsesDAO"""
+        resp = self.get(id)
+        self.responses.remove(resp)
+
+
+DAO = ResponsesDAO()
+DAO.create(data.alex)
+DAO.create(data.aline)
+
+
+@ns.route('/responses')
 class ResponsesList(Resource):
     """ Resource : ResponsesList
 
@@ -38,25 +166,28 @@ class ResponsesList(Resource):
     GET -- Return a list of ALL responses
     POST -- Create a response to the survey
 
-    Infos : V0.0.1 - The survey has only 1 question
-                     We retrieve only 1 answer
+    Infos : V0.0.2 - The survey has 3 questions
+                    We retrieve 1 answer per question !
 
     """
+    @ns.doc('list_responses')
+    @ns.marshal_with(responses_fields, skip_none=True)
     def get(self):
         """ Instance method to READ all responses """
-        return responses_dict
+        return DAO.responses
+
+    @ns.doc('create_responses')
+    @ns.expect(responses_fields)
+    @ns.marshal_with(responses_fields, skip_none=True)
     def post(self):
         """ Instance method to CREATE a new response """
-        args = parser.parse_args()
-        # The if loop allows us to CREATE a new responses if the responses_dict is null
-        if responses_dict.keys():
-            responses_id_num = int(max(responses_dict.keys()).lstrip('resp')) + 1
-        else:
-            responses_id_num = 1
-        responses_id = f"resp{responses_id_num}"
-        responses_dict[responses_id] = {'data': args['data']}
-        return responses_dict[responses_id], 201
+        #args = parser.parse_args() #pas besoin ! on prends un payload
+        return DAO.create(api.payload),201 #Du coup il faut mettre en donnée curl un Json
 
+
+@ns.route('/responses/<responses_id>', endpoint='responses_ep')
+@ns.response(404, 'Responses not found')
+@ns.param('responses_id', 'The responses id')
 class Responses(Resource):
     """ Resource : Responses
 
@@ -67,31 +198,74 @@ class Responses(Resource):
     DEL -- Delete a response
 
     """
+    @ns.doc('Get_responses')
+    @ns.marshal_with(responses_fields, skip_none=True)
+    #Le bug c'est que pour construire l'url il va check "ID" et non"responses_ID"
+    # or je lui dit que la route c'est "response_id " >> attribute="responses_id"
     def get(self, responses_id):
         """ Instance method to READ a response """
-        return responses_dict[responses_id]
-    def put(self, responses_id):
+        return DAO.get(responses_id)
+
+    @ns.doc('update_responses')
+    @ns.marshal_with(responses_fields, skip_none=True)
+    def put(self, responses_id): #Method qui ne changera pas trop
         """ Instance method to UPDATE data of a response """
-        args = parser.parse_args()
-        responses_new = {'data': args['data']}
-        responses_dict[responses_id] = responses_new
-        return responses_new, 201
-    def delete(self, responses_id):
+        return DAO.update(responses_id, api.payload)
+
+    @ns.doc('delete_responses')
+    @ns.response(204,'Delete the response')
+    def delete(self, responses_id): #Method qui ne changera pas trop
         """ Instance method to DELETE a response """
-        del responses_dict[responses_id]
+        DAO.delete(responses_id)
         return '', 204
 
-## API Resources
+
+## API Resources >>> v0.0.1 (deprecated)
 ## Setting the API Resources routing here
-api.add_resource(ResponsesList,'/api/v1/responses')
-api.add_resource(Responses,'/api/v1/responses/<responses_id>')
+#api.add_resource(ResponsesList,'/api/v1/responses')
+#api.add_resource(Responses,'/api/v1/responses/<responses_id>')
 
 
 @app.route("/test")
 def hello():
     """ Test function for the Flask module """
+    text_test = 'Texte secret !'
     return "Hello world!"
 
+#Test du Flask + poll
+@app.route("/index")
+def index():
+    data_index = "Hello World"
+    return render_template('index.html', data=data_index)
+
+@app.route("/survey")
+def survey():
+    """ Test Page with a "Button"
+
+    Description : The button will simulate a sending of patient data
+
+    """
+
+    payload = 'hello world'
+    return render_template('survey.html')
+
+@app.route("/result", methods = ['POST'])
+def result():
+    """
+        Result of pressing the "Button" on /survey page
+
+    description : We want the button to POST data on the API
+    We want to see the result of the POST request onto this page !
+    """
+    if request.method == 'POST':
+        #rv = rq.get('http://localhost:5000/apiv1/responses/resp1')
+        payload = {"questions": [{"q_id":"q100","answer_id":"a100","answer_score":50},{"q_id":"q200","answer_id":"a200","answer_score":40},{"q_id":"q300","answer_id":"a300","answer_score":60}]}
+        url = 'http://localhost:5000/apiv1/responses'
+        rv = rq.post(url,json=payload )
+        rv_content = rv._content.decode("utf-8")
+        rv_date = rv.headers['Date']
+        app.logger.info(rv) # Lecture dans la console de debug de flask
+        return render_template('result.html',data=rv_content, data_2=rv_date),201
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,100 @@
+#! usr/bin/flaskenv python3
+# -*-coding:Utf-8 -*
+import json
+
+"""
+    Declaration of the "model" DTO : Data Transfert object
+
+    Json Model
+    patient DTO
+    {
+        id : string
+        href : Url
+        questions : [ {
+            id : String
+            text : String
+            answer_id : String
+            answer_text : String
+            answer_score : Integer
+                        }]
+    }
+
+
+issues : Dans les examples v2 (pas besoin de déclarer "ID" et "HREF" ca le fait automatiquement)
+
+"""
+
+alex = {
+        #'id': 'resp1',
+        #'href': '/api/responses/r1',
+        'questions': [
+                        {
+                        'q_id':'q1',
+                        'text': 'Vous sentiez vous enthousiaste?',
+                        'answer_id':'a1',
+                        'answer_text':'Tout le temps',
+                        'answer_score': 100
+                        },
+                        {
+                        'q_id':'q2',
+                        'text': 'Avez vous beaucoup d energie ?',
+                        'answer_id': 'a2',
+                        'answer_text': 'Tout le temps',
+                        'answer_score': 100
+                        },
+                        {
+                        'q_id':'q3',
+                        'text': 'Avez vous l impression d etre epuise ?',
+                        'answer_id': 'a3',
+                        'answer_text': 'Jamais',
+                        'answer_score': 100
+                        }
+                    ]
+        }
+
+aline = {
+        #'id': 'resp2',
+        #'href': '/api/responses/r2',
+        'questions': [
+                        {
+                        'q_id':'q10',
+                        'text': 'Vous sentiez vous enthousiaste?',
+                        'answer_id':'a10',
+                        'answer_text':'Jamais',
+                        'answer_score': 0
+                        },
+                        {
+                        'q_id':'q20',
+                        'text': 'Avez vous beaucoup d energie ?',
+                        'answer_id':'a20',
+                        'answer_text':'Jamais',
+                        'answer_score': 0
+                        },
+                        {
+                        'q_id':'q30',
+                        'text': 'Avez vous l impression d etre epuise ?',
+                        'answer_id':'a30',
+                        'answer_text':'Tout le temps',
+                        'answer_score': 0
+                        }
+                    ]
+        }
+
+""" Test pour le 'TestButton' """
+payload = {"questions": [{"q_id":"q100","answer_id":"a100","answer_score":50},{"q_id":"q200","answer_id":"a200","answer_score":40},{"q_id":"q300","answer_id":"a300","answer_score":60}]}
+url = 'http://localhost:5000/apiv1/responses'
+#r = requests.post(url,json=payload )
+#CF Request documentation
+""" Query examples
+
+POST /responses :
+curl http://localhost:5000/apiv1/responses -d '{"questions": [{"q_id":"q100","answer_id":"a100","answer_score":50,"text":null},{"q_id":"q200","answer_id":"a200","answer_score":40},{"q_id":"q300","answer_id":"a300","answer_score":60}]}' -X POST -H "Content-Type: application/json"
+
+curl http://localhost:5000/apiv1/responses -d '{"questions":[{"q_id":"q9999","answer_id":"a9999"}]}' -X POST -H "Content-Type: application/json"
+
+PUT /responses/responsesid
+curl http://localhost:5000/apiv1/responses/resp3 -d '{"questions":[{"q_id":"UPDATED"}]}' -X PUT -H "Content-Type: application/json"
+
+DELETE /responses/responsesid
+curl http://localhost:5000/apiv1/responses/resp3 -X DELETE
+"""
